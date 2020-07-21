9dd6862d13db663a78ac333bd45050b187ba144a
Seth Rubenstein <seth@sethrubenstein.info>
2020-07-21 15:14:58 -0400

    move api init inside construct

--- easyteachlms.php
+++ easyteachlms.php
@@ -57,41 +57,6 @@ define( 'EASYTEACHLMS_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( $easyteach
 define( 'EASYTEACHLMS_VENDOR_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( $easyteach_lms_file ) ) . '/vendor/' );
 define( 'EASYTEACHLMS_URL', plugin_dir_url( __FILE__ ) );
 
-// Instantiate WC_AM_Client class object if the WC_AM_Client class is loaded.
-if ( class_exists( 'WC_AM_Client_2_7' ) ) {
-	/**
-	 * This file is only an example that includes a plugin header, and this code used to instantiate the client object. The variable $wcam_lib
-	 * can be used to access the public properties from the WC_AM_Client class, but $wcam_lib must have a unique name. To find data saved by
-	 * the WC_AM_Client in the options table, search for wc_am_client_{product_id}, so in this example it would be wc_am_client_132967.
-	 *
-	 * All data here is sent to the WooCommerce API Manager API, except for the $software_title, which is used as a title, and menu label, for
-	 * the API Key activation form the client will see.
-	 *
-	 * ****
-	 * NOTE
-	 * ****
-	 * If $product_id is empty, the customer can manually enter the product_id into a form field on the activation screen.
-	 *
-	 * @param string $file             Must be __FILE__ from the root plugin file, or theme functions, file locations.
-	 * @param int    $product_id       Must match the Product ID number (integer) in the product.
-	 * @param string $software_version This product's current software version.
-	 * @param string $plugin_or_theme  'plugin' or 'theme'
-	 * @param string $api_url          The URL to the site that is running the API Manager. Example: https://www.toddlahman.com/
-	 * @param string $software_title   The name, or title, of the product. The title is not sent to the API Manager APIs, but is used for menu titles.
-	 *
-	 * Example:
-	 *
-	 * $wcam_lib = new WC_AM_Client_2_7( $file, $product_id, $software_version, $plugin_or_theme, $api_url, $software_title );
-	 */
-
-	// Example of empty string product_id.
-	// $wcam_lib = new WC_AM_Client_2_7( __FILE__, '', '1.0', 'plugin', 'http://wc/', 'Simple Comments - Simple' );
-	// $wcam_lib = new WC_AM_Client_2_7( __FILE__, '', '1.0', 'plugin', 'http://wc/', 'WooCommerce API Manager PHP Library for Plugins and Themes' );
-
-	// Preferred positive integer product_id.
-	$wcam_lib = new WC_AM_Client_2_7( EASYTEACHLMS_FILE, 139932, EASYTEACHLMS_VERSION, 'plugin', 'https://easyteachlms.com/', 'EasyTeach LMS' );
-}
-
 require_once EASYTEACHLMS_VENDOR_PATH . '/autoload.php';
 use tgmpa\tgmpa;
 use WPackio\Enqueue;
@@ -130,9 +95,43 @@ class EasyTeachLMS {
 	 */
 	public function __construct( $init = false ) {
 		if ( true === $init ) {
-			if ( is_object( $wcam_lib ) && $wcam_lib->get_api_key_status( false ) ) {
-				// Code to load your plugin or theme here.
-				// This code will not run until the API Key is activated.
+			// Instantiate WC_AM_Client class object if the WC_AM_Client class is loaded.
+			if ( class_exists( 'WC_AM_Client_2_7' ) ) {
+				/**
+				 * This file is only an example that includes a plugin header, and this code used to instantiate the client object. The variable $wcam_lib
+				 * can be used to access the public properties from the WC_AM_Client class, but $wcam_lib must have a unique name. To find data saved by
+				 * the WC_AM_Client in the options table, search for wc_am_client_{product_id}, so in this example it would be wc_am_client_132967.
+				 *
+				 * All data here is sent to the WooCommerce API Manager API, except for the $software_title, which is used as a title, and menu label, for
+				 * the API Key activation form the client will see.
+				 *
+				 * ****
+				 * NOTE
+				 * ****
+				 * If $product_id is empty, the customer can manually enter the product_id into a form field on the activation screen.
+				 *
+				 * @param string $file             Must be __FILE__ from the root plugin file, or theme functions, file locations.
+				 * @param int    $product_id       Must match the Product ID number (integer) in the product.
+				 * @param string $software_version This product's current software version.
+				 * @param string $plugin_or_theme  'plugin' or 'theme'
+				 * @param string $api_url          The URL to the site that is running the API Manager. Example: https://www.toddlahman.com/
+				 * @param string $software_title   The name, or title, of the product. The title is not sent to the API Manager APIs, but is used for menu titles.
+				 *
+				 * Example:
+				 *
+				 * $wcam_lib = new WC_AM_Client_2_7( $file, $product_id, $software_version, $plugin_or_theme, $api_url, $software_title );
+				 */
+
+				// Example of empty string product_id.
+				// $wcam_lib = new WC_AM_Client_2_7( __FILE__, '', '1.0', 'plugin', 'http://wc/', 'Simple Comments - Simple' );
+				// $wcam_lib = new WC_AM_Client_2_7( __FILE__, '', '1.0', 'plugin', 'http://wc/', 'WooCommerce API Manager PHP Library for Plugins and Themes' );
+
+				// Preferred positive integer product_id.
+				$wcam_lib = new WC_AM_Client_2_7( EASYTEACHLMS_FILE, 139932, EASYTEACHLMS_VERSION, 'plugin', 'https://easyteachlms.com/', 'EasyTeach LMS' );
+				if ( is_object( $wcam_lib ) && $wcam_lib->get_api_key_status( false ) ) {
+					// Code to load your plugin or theme here.
+					// This code will not run until the API Key is activated.
+				}
 			}
 			add_action( 'init', array( $this, 'semantic_ui_css_loader' ) );
 			add_action( 'init', array( $this, 'rewrite' ) );
